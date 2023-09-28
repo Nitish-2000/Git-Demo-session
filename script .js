d25373fad3fffef8b57e8fd4b8ae2c869f8443e8
Nitish-2000 <nitishmani111@gmail.com>
2023-09-28 21:57:28 +0530

    Demo 3

---
+++ script .js
@@ -0,0 +1,13 @@
+//while loop
+var a = 8;
+var i=1;
+while(a>i)
+{
+    console.log("This number is gtreater");
+    a=a-1;
+}
+var b = new Date();
+var c = b.getFullYear()
+console.log(c);
+var d = b.getDay();
+console.log(d);
